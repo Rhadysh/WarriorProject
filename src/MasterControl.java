1e43a2a5eb3d6929010e7dce13723f14eac79f20
Rhadysh <charlottemehff@gmail.com>
2025-05-14 13:41:00 -0700

    uncommented start

--- src/MasterControl.java
+++ src/MasterControl.java
@@ -5,12 +5,12 @@ public class MasterControl {
 
 	private void start() {
 
-//		Combat combat = new Combat(new CombatContext());
-//		Warrior warriorOne = new StrongWarriorDecorator(new AggressiveWarrior.Builder(12).attack(16).build());
-//		Warrior warriorTwo = new StrongWarriorDecorator(new DefensiveWarrior.Builder(15).defense(25).build());
-//		Warrior winner = combat.duel(warriorOne, warriorTwo);
-//		System.out.println(winner);
-//		System.out.println(warriorOne.calculateAttack());
-//		System.out.println(warriorTwo.calculateDefense());
+		Combat combat = new Combat(new CombatContext());
+		Warrior warriorOne = new StrongWarriorDecorator(new AggressiveWarrior.Builder(12).attack(16).build());
+		Warrior warriorTwo = new StrongWarriorDecorator(new DefensiveWarrior.Builder(15).defense(25).build());
+		Warrior winner = combat.duel(warriorOne, warriorTwo);
+		System.out.println(winner);
+		System.out.println(warriorOne.calculateAttack());
+		System.out.println(warriorTwo.calculateDefense());
 	}
 }
